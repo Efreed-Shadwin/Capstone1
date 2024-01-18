003dd150efac783e59d3e87214ab4775cdcff55b
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2024-01-18 17:12:04 -0600

    got the stuff behaving kind of

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,25 +1,73 @@
-
+let card=""
+let cardList=""
 document.addEventListener("DOMContentLoaded", createItemCard=>{
-  products.forEach(item=>{
+  products.forEach((item, index)=>{
     let itemList=document.getElementById("itemList")
-    itemList.innerHTML= itemList.innerHTML +
-  `<div class="col-lg-4 mb-3">
-      <div class="card flex-item" style="width: 18rem;">
-        <img src="${item.img}" class="card-img-top" alt="${item.name}">
-        <div class="card-body">
-        <h5 class="card-title">C${item.name}</h5>
-        <p class="card-text">${item.description}</p>
-        <a href="#" class="btn btn-primary" id="${item.name}">Add to cart</a>
-      </div>
-    </div>
-  </div>`
+    
+    if(index==0){
+      card=
+        `<div class="row">
+          <div class="card col-4 p-2 m-2" style=width:15rem;height:30rem;>
+            <img src="${item.img}" class="card-img-top" alt="${item.name}">
+            <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <p class="card-text">${item.description}</p>
+              <a href="#" class="btn btn-primary" id="${item.name}">Add to cart</a>
+            </div>  
+          </div>`
+      
+    }else if(index+1==products.length){ 
+      card=
+        `
+          <div class="card col-4 p-2 m-2" style=width:15rem;height:30rem;>
+            <img src="${item.img}" class="card-img-top" alt="${item.name}">
+            <div class="card-body">
+              <h5 class="card-title">${item.name}potato</h5>
+              <p class="card-text">${item.description}</p>
+              <a href="#" class="btn btn-primary" id="${item.name}">Add to cart</a>
+            </div>
+          </div>
+        </div>`
+    }else if((index+1)%3==0){
+      card =
+        `
+          <div class="card col-4 p-2 m-2" style=width:15rem;height:30rem;>
+            <img src="${item.img}" class="card-img-top" alt="${item.name}">
+            <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <p class="card-text">${item.description}</p>
+              <a href="#" class="btn btn-primary" id="${item.name}">Add to cart</a>
+            </div>
+          </div>
+        </div>
+        <div class="row">`
+    }else {
+      card=
+      
+        `<div class="card col-4 p-2 m-2" style=width:15rem;height:30rem;>
+          <img src="${item.img}" class="card-img-top" alt="${item.name}">
+          <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text">${item.description}</p>
+            <a href="#" class="btn btn-primary" id="${item.name}">Add to cart</a>
+          </div>
+        </div>`              
+      
+      
+      
+    }
+     cardList+= card
+    
+    
+  
   })
+  itemList.innerHTML= itemList.innerHTML + cardList
 });
 
-`<li class="list-group-item d-flex justify-content-between lh-sm">
+/*`<li class="list-group-item d-flex justify-content-between lh-sm">
               <div>
                 <h6 class="my-0">${item.name}</h6>
                 <small class="text-body-secondary">${item.description}</small>
               </div>
               <span class="text-body-secondary">${item.price}</span>
-            </li>`;
+            </li>`;*/
